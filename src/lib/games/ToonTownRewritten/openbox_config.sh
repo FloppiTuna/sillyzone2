8aeaf936331c6afc264921862a8040144741a7cf
Griffin <griffin@edforma.app>
2026-02-18 08:32:52 -0600

    uh

--- src/lib/games/ToonTownRewritten/openbox_config.sh
+++ src/lib/games/ToonTownRewritten/openbox_config.sh
@@ -1,4 +1,3 @@
 export DISPLAY=:99
 
 xsetroot -display :99 -solid "#7495FE" &
-xmessage "Just a moment..." -center -display :99 &
--- src/lib/games/ToonTownRewritten/ttr_launch.sh
+++ src/lib/games/ToonTownRewritten/ttr_launch.sh
@@ -1,2 +1,9 @@
-echo "Launching ToonTown Rewritten..."
+echo "=== sillyzone2 ~ ToonTown Rewritten Launcher ==="
+echo "You are: $SILLYZONE_USERNAME"
+echo ""
+echo "NOTE: This script will attempt to automatically load your account credentials from Sillyzone."
+echo "Make sure you put your credentials in the Jellybean launcher on Sillyzone, and that they are valid."
+
+echo "\033[5mDownloading and running ToonTown Rewritten...\033[0m"
+sleep 2
 /launcher/target/release/shticker_book_unwritten --config /launcher/config.json -d
