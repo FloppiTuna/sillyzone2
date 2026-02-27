ff070b9721eb4a29d45583c266e4e1ba9b2f0ce4
Griffin <griffin@edforma.app>
2026-02-27 08:33:03 -0600

    we dont use this anymore

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	server: {
-		proxy: {
-			'/xpra': {
-				target: 'http://localhost:10000',
-				rewrite: (path) => path.replace(/^\/xpra/, ''),
-				ws: true
-			}
-		}
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
